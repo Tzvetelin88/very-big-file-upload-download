6b82f005f6064df122742781b262a863825ded78
Tsvetelin Bleskov <tbleskov@vmware.com>
2023-03-17 13:25:03 +0200

    v1.2.1

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,18 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
-  title: string =
-    'Upload/Download File with Socket IO. Handle network interuption, big files and resumable uploading.';
-  selectedFile: any;
-  name: string = '';
-
   constructor() {}
 
   ngOnInit() {}
-
-  onFileSelect(event: any) {
-    this.selectedFile = event.target.files[0];
-    this.name = this.selectedFile.name;
-    console.log(this.selectedFile);
-  }
 }
